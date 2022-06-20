2976a1e840c5a2d574220ccd5d04c8fbe91305fd
hanys111406 <hanys111406@naver.com>
2022-06-21 08:20:54 +0900

    K

--- lib/main.dart
+++ lib/main.dart
@@ -17,294 +17,135 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("2022-05-20", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black),),
+        leading: Image.asset("C:/Users/user/OneDrive/바탕 화면/logo.png", width:30, height:30),
+        title: const Text("ProTools          Home    Tools    AboutUs    Career          Sign in    ", style: TextStyle(fontSize: 20, color: Colors.black),),
         centerTitle: true,
         elevation: 0.0,
-        backgroundColor: Colors.green,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home'
+        backgroundColor: Colors.white,
+        actions: [Container(
+          width: 80,
+          height: 30,
+          color: Colors.black,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text("sign up",style: TextStyle(fontSize: 20,color:Colors.white)),
+            ],
           ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.favorite),
-              label: 'Favorite'
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              label: 'Settings'
-          ),
+        ),
+          const SizedBox(width: 50,),
         ],
       ),
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const SizedBox(height: 20,),
-            const Text("Hi,MRMR!",style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold),),
-            const SizedBox(width: 50, height: 20,),
+            const SizedBox(height: 80,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                const Text("Find the best ",style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold),),
+                const Text("property",style: TextStyle(fontSize: 50,fontWeight: FontWeight.bold,color: Colors.blue),),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text("in town with ",style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold),),
+                Text("ProTools",style: TextStyle(fontSize: 50,fontWeight: FontWeight.bold,color: Colors.black),),
+              ],
+            ),
+            const SizedBox(width: 50, height: 50,),
             Container(
-              width: 160,height: 160,
-              color: Colors.lightGreen,
+              width: 2500,height: 50,
+              color: Colors.white70,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                   children: const [
-                    Text("Sample Image",style: TextStyle(fontSize: 10,fontWeight: FontWeight.bold),),
+                    Text("city     >      Category     >       % Price range >        image        image     Find it",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
                   ],
               ),
             ),
-            const SizedBox(height: 50,),
+            const SizedBox(height: 30,),
+            SizedBox(
+              child: Image.asset('C:/Users/user/OneDrive/바탕 화면/back.png'),
+              height: 500,
+              width: 500,
+            ),
+            const SizedBox(height: 20,),
+            const SizedBox(width: 50, height: 30,),
             Container(
-              width: 10000, height: 50,
-              color: Colors.lightGreenAccent,
+              width: 5000,
+              height: 300,
+              color: Colors.white24,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text("Our Products...",style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold,color: Colors.lightGreen),),
+                children: [
+                  const Text("We work with trusted partners",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+                  const SizedBox(height: 20,),
+                  const Text("More than 15.000 brand trust ProTools",style: TextStyle(fontSize: 18,color: Colors.grey),),
+                  const SizedBox(width: 50, height: 10,),
+                  SizedBox(
+                    child: Image.asset('C:/Users/user/OneDrive/바탕 화면/logo1.png'),
+                    width: 400,
+                  ),
+                  const SizedBox(height: 10,),
                 ],
               ),
             ),
-            const SizedBox(height: 50,),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          width: 80, height: 80,
-                          color: Colors.lightBlueAccent,
-                        ),
-                        const Text("Smart plant",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-                        const Text("first description"),
-                        const SizedBox(height: 50, width: 200,),
-                      ],
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          width: 80, height: 80,
-                          color: Colors.orange,
-                        ),
-                        const Text("Salad Box",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-                        const Text("second description"),
-                        const SizedBox(height: 50, width: 200,),
-                      ],
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          width: 80, height: 80,
-                          color: Colors.lightGreenAccent,
-                        ),
-                        const Text("Fruit Box",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-                        const Text("third description"),
-                        const SizedBox(height: 50, width: 200,),
-                      ],
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          width: 80, height: 80,
-                          color: Colors.lightBlueAccent,
-                        ),
-                        const Text("Smart plant",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-                        const Text("first description"),
-                        const SizedBox(height: 50, width: 200,),
-                      ],
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          width: 80, height: 80,
-                          color: Colors.orange,
-                        ),
-                        const Text("Salad Box",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-                        const Text("second description"),
-                        const SizedBox(height: 50, width: 200,),
-                      ],
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          width: 80, height: 80,
-                          color: Colors.lightGreenAccent,
-                        ),
-                        const Text("Fruit Box",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-                        const Text("third description"),
-                        const SizedBox(height: 50, width: 200,),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            const SizedBox(width: 50, height: 30,),
-            const Text("             무럭무럭은\n한국디지털미디어고등학교의\n        창업동아리입니다!",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-            const SizedBox(width: 50, height: 40,),
-            const Text("   무럭무럭\n내부 팀 소개",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-            const SizedBox(width: 50, height: 40,),
-            Container(
-              height: 10, width: 30,
-              color: Colors.green,
-            ),
-            const SizedBox(height: 40,),
-            const Text("개발팀",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),),
-            const SizedBox(height: 40,),
-            Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            width: 80, height: 80,
-                            color: Colors.lightBlueAccent,
-                          ),
-                          const SizedBox(width: 200, height: 10,),
-                          const Text("박은서",style: TextStyle(fontSize: 20,),),
-                          const Text("20EB, Team Leader",style: TextStyle(fontSize: 15),),
-                          const SizedBox(height: 80,),
-                        ],
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            width: 80, height: 80,
-                            color: Colors.orange,
-                          ),
-                          const SizedBox(width: 200, height: 10,),
-                          const Text("최호연",style: TextStyle(fontSize: 20,),),
-                          const Text("21EB, Team Member",style: TextStyle(fontSize: 15),),
-                          const SizedBox(height: 80,),
-                        ],
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            width: 80, height: 80,
-                            color: Colors.orange,
-                          ),
-                          const SizedBox(width: 200, height: 10,),
-                          const Text("백가온",style: TextStyle(fontSize: 20,),),
-                          const Text("21HD, Team Meader",style: TextStyle(fontSize: 15),),
-                          const SizedBox(height: 80,),
-                        ],
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            width: 80, height: 80,
-                            color: Colors.lightBlueAccent,
-                          ),
-                          const SizedBox(width: 200, height: 10,),
-                          const Text("한연수",style: TextStyle(fontSize: 20,),),
-                          const Text("21HD, Team Member",style: TextStyle(fontSize: 15),),
-                          const SizedBox(height: 80,),
-                        ],
-                      ),
-                    ],
-                  ),
+            SizedBox(
+              height: 300,
+              child: Row(
+                children: const[
+                  SizedBox(width: 50,),
+                  Text("Discover our\n property.",style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold),),
+                  SizedBox(width: 300,),
+                  Text("Lorelei's dolor sir affect. conspectus advising elif, sed d emus\ntemper incident ut lane et dollar magna aliquots. Ut elm ad minim\nankles;jackals;j;liaised;jsl;darkly;jdk;stifle\naudial;kak;def",style: TextStyle(fontSize: 15,color: Colors.grey),),
                 ],
+              ),
             ),
-            const Text("농작물 판매 관리부",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),),
-            const SizedBox(height: 40,),
-            Column(
+            SizedBox(
+              child: Image.asset('C:/Users/user/OneDrive/바탕 화면/qp.png'),
+              width: 800,
+              height: 400,
+            ),
+            SizedBox(
+              height: 300,
+              child: Row(
+                children: const[
+                  SizedBox(width: 50,),
+                  Text("How it works.",style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold),),
+                  ],
+              ),
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          width: 80, height: 80,
-                          color: Colors.lightBlueAccent,
-                        ),
-                        const SizedBox(width: 200, height: 10,),
-                        const Text("맹성재",style: TextStyle(fontSize: 20,),),
-                        const Text("20EB, Team Leader",style: TextStyle(fontSize: 15),),
-                        const SizedBox(height: 80,),
-                      ],
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          width: 80, height: 80,
-                          color: Colors.orange,
-                        ),
-                        const SizedBox(width: 200, height: 10,),
-                        const Text("신나리",style: TextStyle(fontSize: 20,),),
-                        const Text("20EB, Team Member",style: TextStyle(fontSize: 15),),
-                        const SizedBox(height: 80,),
-                      ],
-                    ),
-                  ],
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.grey,
+                  child: const Text("Search your property"),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          width: 80, height: 80,
-                          color: Colors.orange,
-                        ),
-                        const SizedBox(width: 200, height: 10,),
-                        const Text("최우성",style: TextStyle(fontSize: 20,),),
-                        const Text("21HD, Team Meader",style: TextStyle(fontSize: 15),),
-                        const SizedBox(height: 80,),
-                      ],
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          width: 80, height: 80,
-                          color: Colors.lightBlueAccent,
-                        ),
-                        const SizedBox(width: 200, height: 10,),
-                        const Text("문서연",style: TextStyle(fontSize: 20,),),
-                        const Text("21HD, Team Member",style: TextStyle(fontSize: 15),),
-                        const SizedBox(height: 80,),
-                      ],
-                    ),
-                  ],
-                ),
+                const SizedBox(width: 50,),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.grey,
+                  child: const Text("Search your property"),
+                ),const SizedBox(width: 50,),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.grey,
+                  child: const Text("Search your property"),
+                ),const SizedBox(width: 50,),
               ],
             ),
-          ],
+              ],
+            ),
         ),
-      ),
     );
   }
 }
